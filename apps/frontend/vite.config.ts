e2773aa780321add0959fc8d87905fce3d9a222d
Kevin Derhet <kederhet@fu-r2-p4.s19.be>
2025-09-17 14:29:28 +0200

    oops

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,11 +5,7 @@ export default defineConfig({
 	server: {
 		host: '0.0.0.0',
 		port: 5173,
-<<<<<<< HEAD
 		allowedHosts: true
-=======
-		allowedHosts: ["fu-r2-p4"]
->>>>>>> 5d02d0c (New: Server-side game + refactor)
 	},
 	plugins: [
 		tailwindcss(),
