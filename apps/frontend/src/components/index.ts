a3e04854a812176af43cb910bba3d244c8117d35
JonatV <47326572+JonatV@users.noreply.github.com>
2025-11-29 21:38:10 +0100

    refactor: integrate game3d in the componentRegistry and remove unused Game3DConfig interface

--- apps/frontend/src/components/index.ts
+++ apps/frontend/src/components/index.ts
@@ -1,9 +1,11 @@
 import { PongGame } from './PongGame/PongGame';
+import { Game3D } from './Game3d/Game3d';
 import { ComponentRegistry } from './ComponantManager';
 import { JoinTournamentComponent } from './JoinTournament';
 
 export function registerComponents(): void
 {
     ComponentRegistry.register('pong-game', PongGame)
+	ComponentRegistry.register('game3d', Game3D)
     ComponentRegistry.register('joinTournament', JoinTournamentComponent)
 }
